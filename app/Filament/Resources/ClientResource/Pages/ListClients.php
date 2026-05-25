097cd003ffeb03d1094fbb46438d83da3fd7bc71
peleskov <peleskov@yandex.ru>
2026-05-25 14:39:07 +0200

    UPD страница клиентов в админке на полную ширину (Width::Full)

--- app/Filament/Resources/ClientResource/Pages/ListClients.php
+++ app/Filament/Resources/ClientResource/Pages/ListClients.php
@@ -6,10 +6,16 @@ use Filament\Actions\CreateAction;
 use App\Filament\Resources\ClientResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\Width;
 
 class ListClients extends ListRecords
 {
     protected static string $resource = ClientResource::class;
+
+    public function getMaxContentWidth(): Width|string|null
+    {
+        return Width::Full;
+    }
 
     protected function getHeaderActions(): array
     {
